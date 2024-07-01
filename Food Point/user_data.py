dc819191949b9125702f3a0a3da293a57ed64595
murtaza335 <75629054+murtaza335@users.noreply.github.com>
2024-07-01 16:17:41 +0500

    Add files via upload

---
+++ Food Point/user_data.py
@@ -0,0 +1,9 @@
+cashier_data = [
+    [
+        "murtaza","Muhammad Murtaza","80368036","123456789","Morn"
+    ],
+    [
+        "mujtaba","Muhammad Mujtaba","335335","1232345789","Eve"
+    ]
+
+]
