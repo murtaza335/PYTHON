9e7e770a0c649f420deb6ce2079679d665da29df
murtaza335 <75629054+murtaza335@users.noreply.github.com>
2024-07-09 14:11:35 +0500

    Delete Food Point directory

--- Food Point/user_data.py
+++
@@ -1,9 +0,0 @@
-cashier_data = [
-    [
-        "murtaza","Muhammad Murtaza","80368036","123456789","Morn"
-    ],
-    [
-        "mujtaba","Muhammad Mujtaba","335335","1232345789","Eve"
-    ]
-
-]
